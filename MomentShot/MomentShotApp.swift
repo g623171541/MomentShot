033d518318ddb9d179088b42556b631590346e54
paddygu <wwwhappygxd@163.com>
2026-04-27 10:40:30 +0800

    新建项目

---
+++ MomentShot/MomentShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentShotApp.swift
+//  MomentShot
+//
+//  Created by HET on 2026/4/27.
+//
+
+import SwiftUI
+
+@main
+struct MomentShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
